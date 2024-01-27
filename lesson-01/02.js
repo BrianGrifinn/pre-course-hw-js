75324c376585d677c096b2fcca9b2ec7d2c49f22
Brian Grifinn <briangrifinn@gmail.com>
2024-01-27 16:48:42 +0200

    commit

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,22 @@
-let students = [
-    ["audi", "Slavik", 1998],
-    ["lada", "Ivan", 2004],
-    ["bmw", "Albert", 2020],
+/*let informationClients = [
+    ["Kiril ", "Herson ", 25],
+    ["Karl ", "Kiev ", 50],
+    ["Viktoria ", "Kharkov ", 20],
+]
+
+let list = informationClients[1];
+Client = list[1];
+
+document.write()*/
+
+let usersInfo = [
+    ["Anatolyi: ", "Tayota ", "c 2008"],
+    ["Viktor: ", "Honda ", "c 2020"],
 ];
 
-let list = students[0];
-
+for (let x = 0; x <= usersInfo.length; x++) {
+    let user = usersInfo[0];
+for (let g = 0; user.length; g++) {
+    document.write(user)
+}
+};
